fb0f2582c2268e3938fdb591df7be81c3b832204
R. Potter <rjspotter@gmail.com>
2019-06-10 20:00:37 +0000

    start seeds example

--- spec/van_damme_spec.rb
+++ spec/van_damme_spec.rb
@@ -1,2 +1,13 @@
 RSpec.describe VanDamme do
+  it "does a thing" do
+    expect(true).to be true
+  end
+
+  it "does a second thing" do
+    expect(true).to_not be false
+  end
+
+  it "does a third thing" do
+    expect(true).to be_truthy
+  end
 end
